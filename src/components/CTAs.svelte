45c8169b07496d3854213675234acd095668c1dc
ilanlee <161081945+ilanlee@users.noreply.github.com>
2024-02-23 23:16:26 +0200

    first commit

---
+++ src/components/CTAs.svelte
@@ -0,0 +1,13 @@
+<script>
+
+</script>
+<h1>---CTAS start---</h1>
+<div class="flex items-center gap-4">
+    <button class="specialBtn">
+        <p class="text-base sm:text-lg md:text-xl">Contct Us</p>
+    </button>
+    <button class="specialBtnDark">
+        <p class="text-base sm:text-lg md:text-xl">Get Started &rarr;</p>
+    </button>
+</div>
+<h1>---CTAS end---</h1>
